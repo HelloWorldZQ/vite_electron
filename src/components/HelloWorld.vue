e8fddeaf854ade9c05961ae054f4b163d1925d4d
Quintin Zhang <quintin.zhang@zoom.us>
2023-04-10 23:56:26 +0800

    1.0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <el-button @click="count++">count is: {{ count }}</el-button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
